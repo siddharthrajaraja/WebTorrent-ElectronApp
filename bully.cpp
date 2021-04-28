de0bd30e24bdad0948cf10f5356ccd9c379ad059
siddharth raja <siddharthraja9849@gmail.com>
2021-04-28 20:31:41 +0530

    adding electron app

---
+++ bully.cpp
@@ -0,0 +1,146 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define umap unordered_map
+#define REP(i,a,b) for(int i=a;i<b;i++)
+
+class Bully{
+    private :
+        int id;
+        umap<int,bool>isActive;
+
+    public :
+        Bully(int pid,bool isActive){
+            this->id=pid;
+            this->isActive[pid]=isActive;
+        }
+
+
+};
+int main(){
+    int i;
+    int n;cout<<"ENTER NUMBER OF PROCESS : "; cin>>n;
+    
+    REP(i,0,n){
+        cout<<"IS PROCESS "<<i+1<<" ACTIVE? (yes/no) ";
+        Bully *obj= new Bully(i+1,);
+
+    }   
+    
+    return 0;
+}#include<bits/stdc++.h>
+using namespace std;
+#define umap unordered_map
+#define REP(i,a,b) for(int i=a;i<b;i++)
+
+class Bully{
+    private :
+        int id;
+        umap<int,bool>isActive;
+
+    public :
+        Bully(int pid,bool isActive){
+            this->id=pid;
+            this->isActive[pid]=isActive;
+        }
+
+
+};
+int main(){
+    int i;
+    int n;cout<<"ENTER NUMBER OF PROCESS : "; cin>>n;
+    
+    REP(i,0,n){
+        cout<<"IS PROCESS "<<i+1<<" ACTIVE? (yes/no) ";
+        Bully *obj= new Bully(i+1,);
+
+    }   
+    
+    return 0;
+}#include<bits/stdc++.h>
+using namespace std;
+#define umap unordered_map
+#define REP(i,a,b) for(int i=a;i<b;i++)
+
+class Bully{
+    private :
+        int id;
+        umap<int,bool>isActive;
+
+    public :
+        Bully(int pid,bool isActive){
+            this->id=pid;
+            this->isActive[pid]=isActive;
+        }
+
+
+};
+int main(){
+    int i;
+    int n;cout<<"ENTER NUMBER OF PROCESS : "; cin>>n;
+    
+    REP(i,0,n){
+        cout<<"IS PROCESS "<<i+1<<" ACTIVE? (yes/no) ";
+        Bully *obj= new Bully(i+1,);
+
+    }   
+    
+    return 0;
+}#include<bits/stdc++.h>
+using namespace std;
+#define umap unordered_map
+#define REP(i,a,b) for(int i=a;i<b;i++)
+
+class Bully{
+    private :
+        int id;
+        umap<int,bool>isActive;
+
+    public :
+        Bully(int pid,bool isActive){
+            this->id=pid;
+            this->isActive[pid]=isActive;
+        }
+
+
+};
+int main(){
+    int i;
+    int n;cout<<"ENTER NUMBER OF PROCESS : "; cin>>n;
+    
+    REP(i,0,n){
+        cout<<"IS PROCESS "<<i+1<<" ACTIVE? (yes/no) ";
+        Bully *obj= new Bully(i+1,);
+
+    }   
+    
+    return 0;
+}#include<bits/stdc++.h>
+using namespace std;
+#define umap unordered_map
+#define REP(i,a,b) for(int i=a;i<b;i++)
+
+class Bully{
+    private :
+        int id;
+        umap<int,bool>isActive;
+
+    public :
+        Bully(int pid,bool isActive){
+            this->id=pid;
+            this->isActive[pid]=isActive;
+        }
+
+
+};
+int main(){
+    int i;
+    int n;cout<<"ENTER NUMBER OF PROCESS : "; cin>>n;
+    
+    REP(i,0,n){
+        cout<<"IS PROCESS "<<i+1<<" ACTIVE? (yes/no) ";
+        Bully *obj= new Bully(i+1,);
+
+    }   
+    
+    return 0;
+}
